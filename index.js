28c28650bb1301b52d52ec84a97b2341a08d9636
chrstno040420 <104917222+chrstno040420@users.noreply.github.com>
2022-05-31 11:38:58 -0500

    commit tree ba55a710d74f08e1e7c1ebd0bc9c97c8e8ed4f97

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(name, message= "surprise"){
+    let messages = []
+    for(let i = 0; i < name.length; i++){
+       messages.push("Thank you, " + name[i] + ", for the wonderful " + message +" gift!");
+    }
+    return messages;
+}
+
+
+function countDown(){
+    for(let number = 10; number >=0 ; number--){
+    console.log(number);
+}
+}
